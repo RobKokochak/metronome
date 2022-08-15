9074408451374eb7db6dac4926aa06e09e12e453
Rob Kokochak <64873559+RobKokochak@users.noreply.github.com>
2022-08-15 12:55:12 -0400

    modify script

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const click = new Audio('click.mp3');
 const button = document.getElementById('button');
-let tempo = (60000 / parseInt(document.getElementById("bpm").value));
+let tempo;
+let interval;
 // the timing is ok but not exact - setTimeout is not super reliable for this application
-
-let interval = setInterval(function () {
-  click.play();
-}, tempo);
 
 button.addEventListener("click", function handleClick() {
   tempo = 60000 / parseInt(document.getElementById("bpm").value);
