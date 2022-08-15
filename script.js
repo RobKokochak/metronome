937f24f166e339613c32291b258c2649bf74e2b8
Rob Kokochak <64873559+RobKokochak@users.noreply.github.com>
2022-08-15 12:38:36 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const click = new Audio('click.mp3');
+const button = document.getElementById('button');
+let tempo = (60000 / parseInt(document.getElementById("bpm").value));
+// the timing is ok but not exact - setTimeout is not super reliable for this application
+
+let interval = setInterval(function () {
+  click.play();
+}, tempo);
+
+button.addEventListener("click", function handleClick() {
+  tempo = 60000 / parseInt(document.getElementById("bpm").value);
+  console.log(tempo);
+  clearInterval(interval);
+  interval = setInterval(function () {
+    click.play();
+  }, tempo);
+});
